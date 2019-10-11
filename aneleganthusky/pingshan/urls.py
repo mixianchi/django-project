e008e231bf2937c0a1900fe522585595316f9741
Xin <1071165895@qq.com>
2019-10-11 17:20:40 +0800

    django 2

--- aneleganthusky/pingshan/urls.py
+++ aneleganthusky/pingshan/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='pingshan-home'),
+    path('about/', views.about, name='pingshan-about'),
 ]
--- aneleganthusky/pingshan/views.py
+++ aneleganthusky/pingshan/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+def home(request):
+    return HttpResponse('<h1>Pingshan Home</h1>')
 
+def about(request):
+    return HttpResponse('<h1>Pingshan About</h1>')
+"""
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+"""
 
 # Create your views here.
